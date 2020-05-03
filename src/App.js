532eafd73e74955e0292d0e77a4619911285c04b
ifrimenco <ifrimenco.alex@gmail.com>
2020-05-03 18:23:13 +0300

    test

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import React from 'react';
+
+function App() {
+    return (
+        <div>    
+            <h1>
+                Test
+            </h1>
+            <ul>
+                <li>
+                    a
+                </li>
+                <li>
+                    b
+                </li>
+                <li>
+                    c
+                </li>
+            </ul>
+        </div>
+    )
+}
+
+
+export default App;
